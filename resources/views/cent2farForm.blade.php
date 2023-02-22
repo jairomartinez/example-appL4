55dcb3c3052ff577a4632e20924e05ff420456fa
Jairo Martinez <jairo.martinez@unah.edu.hn>
2023-02-22 13:00:12 -0600

    Bootstrap y plantilla

--- resources/views/cent2farForm.blade.php
+++ resources/views/cent2farForm.blade.php
@@ -1,38 +1,38 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends("layout")
+@section("title", "Hola")
+@section("contenedor")
+      <DIV class="row g-3">
+        <div class="col-sm-4 offset-4">
+          <h1> Convertir a Farenheit</h1>
+        
+      
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <nav>
-        <ul>
-            <li>Convertir Centigrados</li>
-            <li>A Farenheit</li>
-            <li>A Kelvin</li>
-            <li>Diferencia</li>
-
-        </ul>
-    </nav>
-
-    <div class="container">
-        <h1> Convertir a Farenheit</h1>
         <form action="/" method="POST">
-            <input type="number" name="centigrados" placeholder="Ingrese grados cenrigrados">
-            <input type="text" disabled>
-            <input type="submit" value="Convertir">
+        <DIV class="row g-3">
+          <div class="col">
+          <div class="form-floating mb-3">
+            <input type="number" id="centigrados"name="centigrados" class="form-control" placeholder="Ingrese grados cenrigrados" aria-label="Username">      
+            <label for="centigrados">Grados centigrados</label>
+          </div>
+        </div>
+
+        <div class="row g-3">
+          <div class="col">
+            <div class="form-floating mb-3">
+              <input id="farenheit" readonly type="text" name="farenheit" class="form-control" placeholder="Ingrese grados cenrigrados" aria-label="Username">      
+              <label for="farenheit">Farenheit</label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Convertir</button>
+        </DIV>
+        
+
+        
+       
+            
         </form>
     </div>
- 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
-</html>
+  </div>
+  </DIV>
+@endsection
